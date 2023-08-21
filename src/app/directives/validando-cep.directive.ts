3de7bb38e4c4ba53746e897b6a25382cc023c2f5
Edmir Lôbo <ejuniorls@outlook.com>
2023-08-21 08:57:40 -0300

    aula 05 - projeto final

---
+++ src/app/directives/validando-cep.directive.ts
@@ -0,0 +1,27 @@
+import {Directive} from '@angular/core';
+import {ConsultaCepService} from "../service/consulta-cep.service";
+import {AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS, ValidationErrors} from "@angular/forms";
+import {map, Observable} from "rxjs";
+
+@Directive({
+    selector: '[appValidandoCep]',
+    providers: [{
+        provide: NG_ASYNC_VALIDATORS,
+        useExisting: ValidandoCepDirective,
+        multi: true
+    }]
+})
+export class ValidandoCepDirective implements AsyncValidator {
+
+    constructor(private consultaCepService: ConsultaCepService) {
+    }
+
+    validate(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+        const cep = control.value;
+
+        return this.consultaCepService.getConsultaCep(cep).pipe(map(
+            (resultado: any) => resultado.erro ? {'validadorCep': true} : null
+        ))
+    }
+
+}
